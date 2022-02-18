942a46ba2c74cbe2fde2bd1e7197aaa23497f95d
ekinkoseoglu <ekindenizkoseoglu@gmail.com>
2022-02-18 21:07:48 +0300

    Add Components and Register Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarComponent } from './components/car/car.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    BrandComponent,
+    ColorComponent,
+    NaviComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/models/car.ts
@@ -0,0 +1,9 @@
+export interface Car {
+  Id: number;
+  CarName: string;
+  BrandId: number;
+  ColorId: number;
+  ModelYear: number;
+  DailyPrice: number;
+  Description: string;
+}
